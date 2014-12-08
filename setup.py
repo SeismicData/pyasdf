60b20f26ed78bf5f612f5ca8816149c8a6334347
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-12-08 13:31:31 -0500

    Renaming to pyasdf and BSD license

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Setup script for obspy_asdf module.
+Setup script for pyasdf module.
 
 :copyright:
     Lion Krischer (krischer@geophysik.uni-muenchen.de), 2014
 :license:
-    GNU General Public License, Version 3
-    (http://www.gnu.org/copyleft/gpl.html)
+    BSD 3-Clause ("BSD New" or "BSD Simplified")
 """
 import inspect
 import os
@@ -17,13 +16,13 @@ from setuptools import setup, find_packages
 
 def get_package_data():
     """
-    Returns a list of all files needed for the installation relativ to the
-    "obspy_asdf" subfolder.
+    Returns a list of all files needed for the installation relative to the
+    'pyasdf' subfolder.
     """
     filenames = []
     # The lasif root dir.
     root_dir = os.path.join(os.path.dirname(os.path.abspath(
-        inspect.getfile(inspect.currentframe()))), "obspy_asdf")
+        inspect.getfile(inspect.currentframe()))), "pyasdf")
     # Recursively include all files in these folders:
     folders = [os.path.join(root_dir, "tests", "data")]
     for folder in folders:
@@ -39,20 +38,20 @@ def get_package_data():
 
 
 setup_config = dict(
-    name="obspy_asdf",
+    name="pyasdf",
     version="0.0.1a",
     description="Module for creating and processing ASDF files.",
     author="Lion Krischer",
     author_email="krischer@geophysik.uni-muenchen.de",
-    url="http: //github.com/krischer/ASDF",
+    url="http: //github.com/SeismicData/pyasdf",
     packages=find_packages(),
-    license="GNU Lesser General Public License, version 3 (LGPLv3)",
+    license="BSD",
     platforms="OS Independent",
     install_requires=["obspy >=0.9.2", "h5py", "colorama"],
     extras_require={
         "tests": ["pytest", "flake8"]},
     package_data={
-        "obspy_asdf": get_package_data()},
+        "pyasdf": get_package_data()},
 )
 
 
