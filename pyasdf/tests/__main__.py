2b0a395d59af9f610269dfd8e0b87119910b0718
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-04-21 16:10:21 +0200

    Ability to run `python -m pyasdf.tests`

---
+++ pyasdf/tests/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+The only purpose of this file is to be able to run the pyasdf test suite with
+
+python -m pyasdf.tests
+
+:copyright:
+    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2015
+:license:
+    GNU General Public License, Version 3
+    (http://www.gnu.org/copyleft/gpl.html)
+"""
+if __name__ == "__main__":
+    import inspect
+    import os
+    import pytest
+    import sys
+    PATH = os.path.dirname(os.path.dirname(
+        os.path.abspath(inspect.getfile(inspect.currentframe()))))
+
+    sys.exit(pytest.main(PATH))
