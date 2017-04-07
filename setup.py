3e5e58c45e424b65c964d0623b6c7edf0108466f
Lion Krischer <lion.krischer@gmail.com>
2017-04-07 17:58:52 +0200

    Back to a development installation.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ def get_package_data():
 
 setup_config = dict(
     name="pyasdf",
-    version="0.2.0",
+    version="0.2.x",
     description=DOCSTRING[0],
     long_description="\n".join(DOCSTRING),
     author="Lion Krischer",
