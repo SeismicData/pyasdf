1664fd978d7f689a7f7f84643a62e4122cb7846e
Lion Krischer <lion.krischer@gmail.com>
2018-04-06 09:00:00 +0200

    Chaning version number to 0.4.x

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ def get_package_data():
 
 setup_config = dict(
     name="pyasdf",
-    version="0.4.0",
+    version="0.4.x",
     description=DOCSTRING[0],
     long_description="\n".join(DOCSTRING),
     author="Lion Krischer",
